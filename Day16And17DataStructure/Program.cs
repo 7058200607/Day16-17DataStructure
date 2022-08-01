645b7ef85553143ab9bd778d6f51eae259b7b0d0
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-01 14:51:59 +0530

    Insertion Sort to sort the words in the String array

---
+++ Day16And17DataStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16And17DataStructure
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            int[] arr = { 12, 11, 13, 5, 6 };
+            InsertionSort ob = new InsertionSort();
+            ob.sort(arr);
+            InsertionSort ob1 = new InsertionSort();
+            ob1.printArray(arr);
+        }
+    }
+}
